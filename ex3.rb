67a5166c5717fa50ab9a71ddfa12a9c40fe3c414
Ken Lee <ken@newtypecentral.com>
2011-12-02 21:47:07 -0700

    initial commit

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+# This line will display all copy between the quotation marks
+puts "I will now count my chickens:"
+
+# This line will display copy between the quotation marks and calculate 25 plus 30 divided by 6
+puts "Hens", 25.0 + 30.0 / 6.0
+# This line will display copy between the quotes and calculate 100 minus 25 times 3 percent 4
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# This line will display all copy between the quotation marks
+puts "Now I will count the eggs:"
+
+# This line will calculate 3 plus 2 plus 1 minus 5 plus 4 percent 2 minus 1/4 plus 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This line will display copy between the quotation marks
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# This line will calculate 3 plus 2 is less than 5 minus 7
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# This line will display copy between the quotation marks and calculate 3 plus 2
+puts "What is 3 + 2?", 3.0 + 2.0
+# This line will display copy between the quotation marks and calculate 5 minus 7
+puts "What is 5 - 7?", 5.0 - 7.0
+
+# This line will display copy between quotation marks
+puts "How about some more."
+
+# This line will display the copy between quotation marks and calculate 5 is greater than -2 as "True"
+puts "Is it greater?", 5.0 > -2.0
+# This line will display the copy between quotation marks and calculate 5 is greater or equal to -2 as "True"
+puts "Is it greater or equal?", 5.0 >= -2.0
+# This line will display copy between quotation marks and calculated 5 is less then or equal to -2 as "False"
+puts "Is it less or equal?", 5.0 <= -2.0
---
+++ ex3extracredit3.rb
@@ -0,0 +1,6 @@
+# This is the extra credit number 3 for exercise 3
+# Find something to calculate and make a file to do it
+
+puts "This will calculate ramdom math equation I think of."
+
+puts "What does 5 time 5 equal?", 5 * 5 
